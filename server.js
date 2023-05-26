4e6a3beb67f7c06d9fc3b11d32a578534fb19c31
pankaj <pnkjvshsh1@gmail.com>
2023-05-27 01:26:55 +0530

    done

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 // server.js
+
+const path = require('path');
+
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+
+
+const fs = require('fs')
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+const router = jsonServer.router(db)
+
+// const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
  
+
+
 server.use(middlewares)
 server.use('', router)
 server.listen(process.env.PORT || 5000, () => {
